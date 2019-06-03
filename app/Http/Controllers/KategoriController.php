df4002af68b4d072f3f6e56405c1d91fcdc841e4
AnasMisbahuddin <anas.misbahuddin4869@gmail.com>
2019-06-03 14:12:05 +0800

    add template

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -3,8 +3,136 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Kategori;
+use App\Http\Resources\KategoriResource;
 
 class KategoriController extends Controller
 {
-    //
+     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('kategori.index');
+    }
+
+    public function getAllKategori()
+    {
+        return KategoriResource::collection(Kategori::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama'=>'required|min:3|max:100',
+            'deskripsi'=>'required|min:5|max:255'
+        ]);
+
+        $kategori = Kategori::create($request->all());
+        if ($kategori) {
+            return response()->json([
+                'success'=>true,
+                'type'=> 'add',
+                'message'=>'Berhasil Menambahkan Kategori'
+            ]);
+        }
+        return response()->json([
+            'success'=>false,
+            'type'=> 'add',
+            'message'=>'Gagal Menambahkan Kategori'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $kategori = Kategori::findOrFail($id);
+
+        return new KategoriResource($kategori);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama'=>'required|min:3|max:100',
+            'deskripsi'=>'required|min:5|max:255'
+        ]);
+        $updatekategori = Kategori::where('id',$id)->update($request->all());
+        if ($updatekategori) {
+            return response()->json([
+                'success'=>true,
+                'type'=> 'update',
+                'message'=>'Berhasil Mengubah Kategori'
+            ]);
+        }
+        return response()->json([
+            'success'=>false,
+            'type'=> 'update',
+            'message'=>'Gagal Mengubah Kategori'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+            $deletekategori = Kategori::where('id',$id)->delete();
+            if ($deletekategori) {
+                return response()->json([
+                    'success'=>true,
+                    'type'=> 'delete',
+                    'message'=>'Berhasil Menghapus Kategori'
+                ]);
+            }
+            return response()->json([
+                'success'=>false,
+                'type'=> 'delete',
+                'message'=>'Gagal Menghapus Kategori'
+            ]);
+    }
 }
---
+++ app/Http/Resources/KategoriResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class KategoriResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'=>$this->id,
+            'nama' => $this->nama,
+            'deskripsi'=>$this->deskripsi,
+            'action'=>'<a data-toggle="modal" data-target="#myModal" class="btn btn-warning btn-sm btn-edit" id="'.$this->id.'">Edit</a>
+             <a class="btn btn-sm btn-danger btn-delete" id="'.$this->id.'">Hapus</a>'
+        ];
+    }
+}
